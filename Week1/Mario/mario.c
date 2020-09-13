839e70550db01395f83efe6d445de1e4c4e52148
Alyssa Donchu <55606256+AlyssaDonchu@users.noreply.github.com>
2020-09-12 22:26:19 -0700

    Create mario.c

---
+++ Week1/Mario/mario.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int width = get_int("How tall is the piramide?\n") + 1;
+    int count_spaces = width;
+    int count1 = 1;
+    int count2 = 1;
+    //Checks of the original user input (width) is between 1 and 8
+    if (width > 1 && width <= 9)
+    {
+        //counting the height of the piramide
+        while (count1 != width)
+        {
+            //counting and printing spaces first
+            count_spaces = width - count1;
+            while (count_spaces != 1)
+            {
+                printf(" ");
+                count_spaces--;
+            }
+            //counting and printing "#""
+            while (count2 != 0)
+            {
+                printf("#");
+                count2--;
+            }
+            //printing a separator - 2 spaces
+            printf("  ");
+            count2 = count1;
+            //counting and printing "#"" again
+            while (count2 != 0)
+            {
+                printf("#");
+                count2--;
+            }
+            //printing a new line and add 1 to the variable "count1"
+            printf("\n");
+            count1++;
+            count2 = count1;
+        }
+    }
+    //if user input is not between 1 and 8, run the programm again
+    else
+    {
+        main();
+    }
+}
