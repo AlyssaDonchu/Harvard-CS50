89fb32bddd664893b50d737a990455c39d180358
Alyssa Donchu <55606256+AlyssaDonchu@users.noreply.github.com>
2020-09-12 22:25:31 -0700

    Update hello.c

--- Week1/hello.c
+++ Week1/hello.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    //Variable with the type string that asks for users name
+    //Variable with the type string that asks for user's name
     string name = get_string("What's your name?\n");
     //function from standart input output library that prints "hello" and the name of the user
     printf("hello, %s\n", name);
